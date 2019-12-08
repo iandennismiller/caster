4184cc83446268cc5007f74bccf4f2b3166bceba
Ian Dennis Miller <ian@iandennismiller.com>
2019-12-08 11:22:25 -0500

    progress on sbs

--- README.md
+++ README.md
@@ -10,3 +10,4 @@ http://$HOST:8080/
 ffmpeg -i space.mp4 -vcodec libx264 -tune zerolatency -crf 18 http://localhost:1234/feed1.ffm
 ```
 
+rtsp://127.0.0.1:8554/test1.sdp
--- bin/stream-sbs.sh
+++ bin/stream-sbs.sh
@@ -2,7 +2,5 @@
 
 ffmpeg \
     -i http://127.0.0.1:8081/ \
-    -vf stereo3d=sbs2l:abl \
-    -tune zerolatency \
-    -crf 18 \
+    -vf stereo3d=al:sbsl \
     http://localhost:1234/feed1.ffm
