cedd209c3b676731ef3b0695a02eec6496ee8679
Ian Dennis Miller <ian@iandennismiller.com>
2019-12-14 16:09:43 -0500

    side-by-side maker works

--- bin/convert-to-sbs.sh
+++ bin/convert-to-sbs.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-if [ -z $1 || -z $2 ]; then
+if [[ -z $1 || -z $2 ]]; then
     echo "convert-to-sbs.sh INPUT OUTPUT"
     exit
 fi
 
+WIDTH=$(ffprobe -v error -show_entries stream=width -of csv=s=x:p=0 $1)
+HEIGHT=$(ffprobe -v error -show_entries stream=height -of csv=s=x:p=0 $1)
+
 ffmpeg \
     -i "$1" \
-    -vf stereo3d=al:sbsl \
+    -filter:v "scale=-1:720,crop=640:720,stereo3d=al:sbsl" \
     "$2"
