fad9c453a41bb8d678354f0363a2f2c93ba5ae90
Ian Dennis Miller <ian@iandennismiller.com>
2019-12-14 13:36:33 -0500

    analyze files

--- bin/curate-playlist.py
+++ bin/curate-playlist.py
@@ -6,13 +6,7 @@ import glob
 import json
 import click
 
-@click.group()
-def cli():
-    pass
-
-@click.command('generate', short_help='Generate data.')
-@click.option('--config', default='etc/example-config.json', help='Configuration filename.')
-def generate(config):
+def load_cfg(config):
     try:
         with open(config, 'r') as f:
             cfg = json.load(f)
@@ -23,7 +17,45 @@ def generate(config):
         print("Error: cannot parse {}".format(config))
         print(e)
         return(-1)
+    return(cfg)
 
+@click.group()
+def cli():
+    pass
+
+@click.command('analyze', short_help='Analyze path.')
+@click.option('--config', default='etc/example-config.json', help='Configuration filename.')
+def analyze(config):
+    cfg = load_cfg(config)
+
+    import nltk
+    from nltk import ngrams, FreqDist
+    from nltk.corpus import stopwords
+
+    word_list = []
+    for search_path in cfg['subdirs']:
+        search_glob = "{}/{}/**".format(cfg["path"], search_path)
+        for filename in glob.iglob(search_glob, recursive=True):
+            word_list += re.split(r'\W', filename.lower())
+
+    filtered_words = [word for word in word_list if word not in stopwords.words('english')]
+    filtered_words = [word for word in filtered_words if word not in ['vids', '_db', 'mp4', '_', '1', '2']]
+
+    raw = " ".join(filtered_words)
+    bag = nltk.word_tokenize(raw)
+    freqdist = FreqDist(bag)
+    # words = [ x for (x, c) in freqdist.items() if c > 5 ]
+    words_sorted = sorted(freqdist.items(), key = 
+        lambda kv:(kv[1], kv[0]))
+    top_words = words_sorted[-30:]
+    top_words.reverse()
+    for word in top_words:
+        print("{1}: {0}".format(*word))
+
+@click.command('generate', short_help='Generate data.')
+@click.option('--config', default='etc/example-config.json', help='Configuration filename.')
+def generate(config):
+    cfg = load_cfg(config)
     base_cwd = os.getcwd()
 
     for name, pattern in cfg['patterns'].items():
@@ -57,6 +89,7 @@ def generate(config):
 
 
 cli.add_command(generate)
+cli.add_command(analyze)
 
 if __name__ == '__main__':
     print("curate 0.1")
